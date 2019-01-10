789e76e2df19fbaa92af8780156692cdf418449a
Jamais <wuweikang@cmcm.com>
2019-01-10 22:20:35 +0800

    Remove gif and fix npmignore file

--- demo/index.js
+++ demo/index.js
@@ -4,9 +4,6 @@ const img03 = require('./images/03.png');
 const img04 = require('./images/04.jpg');
 const img05 = require('./images/05.jpg');
 const img06 = require('./images/06.jpg');
-const img07 = require('./images/07.gif');
-const img08 = require('./images/08.gif');
-const img09 = require('./images/09.gif');
 
 
-console.log('Image imported', img01, img02, img03, img04, img05, img06, img07, img08, img09);
+console.log('Image imported', img01, img02, img03, img04, img05, img06);
